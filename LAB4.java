9cca0097559f6c24b019a970706e5c605dd6fc02
NTPLSRPP <110714151+NTPLSRPP@users.noreply.github.com>
2023-04-19 12:31:17 +0700

    Add files via upload

--- LAB4.java
+++ LAB4.java
@@ -1,1 +1,34 @@
+package bytestream1;
 
+import java.io.*;
+import java.util.logging.Logger;
+
+public class LAB4 {
+    public static void main(String[] args) {
+        String s = "Hello World!";
+        byte[] b = s.getBytes();
+        try {
+            FileOutputStream file = new FileOutputStream("data.dat");
+            BufferedOutputStream output = new BufferedOutputStream(file);
+            output.write(b);
+            output.close();
+        } catch (FileNotFoundException e) {
+            Logger.getLogger(LAB4.class.getName()).log(null, "File not found");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            FileInputStream file = new FileInputStream("data.dat");
+            BufferedInputStream input = new BufferedInputStream(file);
+            int ch;
+            while ((ch = input.read()) != -1) {
+                System.out.print((char)ch);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
